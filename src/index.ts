feddcdad6eefd53123b64dacf47c60f05657caea
nolash <dev@holbrook.no>
2019-06-22 00:55:19 +0200

    WIP post and retrieve tmp privkey

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,92 @@
-import { BzzAPI } from '@erebos/swarm';
-import { createKeyPair, sign } from '@erebos/secp256k1'
-import { pubKeyToAddress } from '@erebos/keccak256'
+import { createHex, BzzAPI  } from '@erebos/swarm';
+import { createKeyPair, createPublic, sign } from '@erebos/secp256k1'
+import { pubKeyToAddress, hash } from '@erebos/keccak256'
 
+var GATEWAY_URL = 'http://localhost:8500';
+
+// yes, we use globalsss!
+var keyTmpRequestPriv = "";
+var keyOtherPub = "";
+
+
+// us
 var keyPairSelf = createKeyPair();
-var keyPairTemp = createKeyPair();
-var pubkey = keyPairSelf.getPublic();
-var pubkeyHex = pubkey.encode("hex", false);
-//var user = pubKeyToAddress(pubkeyHex);
-var signBytes = async bytes => sign(bytes, keyPairSelf.getPrivate());
-var bzz = new BzzAPI({ url: 'https://swarm-gateways.net', signBytes });
-console.log(signBytes);
-console.log(bzz);
+var userSelf = pubKeyToAddress(createHex("0x" + keyPairSelf.getPublic("hex")));
+var signerSelf = async bytes => sign(bytes, keyPairSelf.getPrivate());
 
-export function uploadToFeed(topic, data) {//user, topic, data) {
-	var feedOptions = {
-//		user,
-		topic,
-	}
-	return bzz.uploadFeedValue(feedOptions, data)
+
+// the handshake feed 
+var keyPairTmp = createKeyPair(keyTmpRequestPriv);
+var keyTmpPub = keyPairTmp.getPublic("hex");
+var userTmp = pubKeyToAddress(createHex("0x" + keyTmpPub));
+var topicTmp = "0x";
+// BUG: createHex doesn't work for the hash output, annoying!
+var topicTmpArray = hash(Buffer.from(keyPairTmp.getPrivate("hex"))); 
+topicTmpArray.forEach(function(k) {
+	var s = "00" + Math.abs(k).toString(16);
+	topicTmp += s.substring(s.length-2, s.length);
+	
+});
+var signerTmp = async bytes => sign(bytes, keyPairTmp.getPrivate());
+var bzr = new BzzAPI({ url: GATEWAY_URL });
+// the peer
+var keyPairOtherPub = undefined;
+if (keyOtherPub != "") {
+	keyPairOtherPub = createPublic(keyOtherPub);
 }
 
+console.log("topic: " + topicTmp);
+console.log("user self: " + userSelf);
+console.log("tmp priv: " + keyPairTmp.getPrivate("hex"));
+console.log("pub self: " + keyPairSelf.getPublic("hex"));
+console.log("user other: " + userSelf);
+
+function uploadToFeed(user, signer, topic, data) {//user, topic, data) {
+
+	// BUG: why does signBytes have to be named "signBytes"?
+	var signBytes = signer;
+	var bz = new BzzAPI({ url: GATEWAY_URL,  signBytes });
+	var feedOptions = {
+		user: user,
+		topic: topic,
+	}
+	console.log(bz);
+	
+	return bz.setFeedContent(feedOptions, data);
+}
+
+function downloadFromFeed(user, topic) {
+	var feedOptions = {
+		user: user,
+		topic: topic,
+	}
+	console.log(bzr);
+
+	return bzr.getFeedContent(feedOptions, {
+		mode: "raw",
+	});
+}
+
+// Handle the handshake from the peer that responds to the invitation
+function handshakeResponse() {
+	return new Promise(function(whohoo, doh) {
+		whohoo();
+	});
+}
+
+function handshakeRequest() {
+	return new Promise(function(whohoo, doh) {
+		uploadToFeed(userTmp, signerTmp, topicTmp, keyPairTmp.getPrivate("hex")).then(function(k) {
+			console.log("feed hash: " + k);
+			downloadFromFeed(userTmp, topicTmp).then(function(e) {
+				e.text().then(function(b) {
+					whohoo(b);	
+				});
+			});
+		});
+	});
+}
+
+handshakeRequest().then(function(h) {
+	console.log("ok:" + h);
+});
