90c3407b2a2bfd3cd1f6cb63745be590858afc03
Attila Gazso <agazso@gmail.com>
2019-11-14 15:58:21 +0100

    Improved upload script

--- upload.sh
+++ upload.sh
@@ -1,20 +1,25 @@
 #!/bin/sh -e
 
-bzzapi="--bzzapi https://swarm-gateways.net"
+gateway="https://swarm-gateways.net"
 if [ "$1" != "" ]; then
-    bzzapi="--bzzapi $1"
+    gateway="$1"
 fi
-alias sw="swarm $bzzapi"
+echo "Gateway: $gateway"
+password=""
+if [ -f password ]; then
+    password="--password password"
+fi
+alias sw="swarm --bzzapi $gateway"
 
 bzzaccount="0x5660eb1fc0fa1abc2bef8d50fe7a2e094490d501"
 html_hash=$(sw up dist/index.html)
 html_topic="0x646973706f636861745f68746d6c000000000000000000000000000000000000"
 
-sw --bzzaccount "$bzzaccount" feed update --topic "$html_topic" "0x$html_hash"
+sw $password --bzzaccount "$bzzaccount" feed update --topic "$html_topic" "0x$html_hash"
 
 script_hash=$(sw up dist/index.js)
 script_topic="0x646973706f636861745f73637269707400000000000000000000000000000000"
 
-sw --bzzaccount "$bzzaccount" feed update --topic "$script_topic" "0x$script_hash"
+sw $password --bzzaccount "$bzzaccount" feed update --topic "$script_topic" "0x$script_hash"
 
 sw --recursive --defaultpath dist/index.html up dist
