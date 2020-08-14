2903f8e229bfbc3fbbc1d951443588465d16a3ce
Attila Gazso <agazso@gmail.com>
2020-08-14 13:13:53 +0200

    Add nodejs crypto polyfill

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,13 @@
 import { init } from './index';
 
+if ((typeof process !== 'undefined') && (typeof process.versions.node !== 'undefined')) {
+	const { Crypto } = require("node" + "-webcrypto-ossl");
+	global.crypto = new Crypto({
+		directory: "key_storage"
+	})
+}
+
+
 // set up the session object
 function logMessage(msg) {
 	console.debug("got message", msg.payload());
